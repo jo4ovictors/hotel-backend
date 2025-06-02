93a6a27f848c74e71fe3e90b5af2184682514d9c
jo4ovictors <joaovictors1003@gmail.com>
2025-06-02 09:16:11 -0300

    criação do projeto

---
+++ src/main/java/edu/ifmg/hotelbao/HotelbaoApplication.java
@@ -0,0 +1,13 @@
+package edu.ifmg.hotelbao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelbaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelbaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ifmg/hotelbao/HotelbaoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ifmg.hotelbao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelbaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
